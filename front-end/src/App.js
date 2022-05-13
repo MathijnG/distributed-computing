75741014b361dc3ab2c3ef84d0838d7ae36504a5
MathijnG <mathijngoossens@gmail.com>
2022-05-13 13:22:27 +0200

    Added submitting file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,29 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Container,Row,Button,Col,Form} from "react-bootstrap";
 import CodeEditor from '@uiw/react-textarea-code-editor';
 import axios from "axios";
 
 function App() {
 
-  const [code, setCode] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
 
-  const submitCode = () => {
-    axios.post("/api/submit", {code: code})
+  const submitCode = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("selectedFile", selectedFile);
+
+    axios.post("/api/submit", formData, {headers: {"Content-Type": "multipart/form-data"}})
       .then((response)=>{
-        console.log(response);
+        //console.log(response);
       })
       .catch((error)=>{
-        console.log(error)
+        //console.log(error)
       })
+  }
+
+  const handleFileSelect = (e) => {
+    setSelectedFile(e.target.files[0]);
   }
 
   return (
@@ -22,30 +31,15 @@ function App() {
       <Container style={{padding: "5rem"}}>
         <Row>
           <Col style={{padding: "3rem"}}>
-            
-              {/* <CodeEditor
-                value={code}
-                language="python"
-                placeholder="Please enter python code here."
-                onChange={(e) => setCode(e.target.value)}
-                padding={15}
-                style={{
-                  borderRadius: 6,
-                  fontSize: 15,
-                  backgroundColor: "#1C1B22",
-                  fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-                  marginBottom: "1rem"
-                }}
-              /> */}
-
-              <Form.Group className="mb-3">
-                <Form.Control type="file" />
-              </Form.Group>
-
-              <div>
-                <Button variant="dark" style={{width:"30%"}} onClick={()=>submitCode()}>Submit to cluster</Button>
-              </div>
-            
+              <p>The code you submit will be distributed on a cluster of several workstations to improve performance.</p>
+              <Form>
+                <Form.Group className="mb-3">
+                  <Form.Control type="file" accept=".py" onChange={handleFileSelect} />
+                </Form.Group>
+                <div>
+                  <Button type="submit" variant="dark" style={{width:"30%"}} onClick={(e)=>submitCode(e)}>Submit to cluster</Button>
+                </div>
+              </Form>
           </Col>
         </Row>
       </Container>
