70933540bd0fc4a3c8b762386f918837859f700b
MathijnG <mathijngoossens@gmail.com>
2022-05-18 10:39:36 +0200

    Changed to string based python submit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,31 +1,39 @@
 import {useEffect, useState} from "react";
-import {Container,Row,Button,Col,Form} from "react-bootstrap";
+import {Container,Row,Button,Col,Form, Alert} from "react-bootstrap";
 import CodeEditor from '@uiw/react-textarea-code-editor';
 import axios from "axios";
 
 function App() {
 
   const [code, setCode] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const submitCode = (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    formData.append("selectedFile", selectedFile);
+    if (code && title) {
+      setSuccess(true);
+      setError(false);
 
-    axios.post("/api/submit", formData, {headers: {"Content-Type": "multipart/form-data"}})
-      .then((response)=>{
-        //console.log(response);
-        //setCode(response)
-      })
-      .catch((error)=>{
-        //console.log(error)
-      })
-  }
+      const formData = new FormData();
+      formData.append("title", title)
+      formData.append("code", code);
 
-  const handleFileSelect = (e) => {
-    setSelectedFile(e.target.files[0]);
+      axios.post("/api/submit", formData, {headers: {"Content-Type": "multipart/form-data"}})
+        .then((response)=>{
+          //console.log(response);
+          //setCode(response)
+        })
+        .catch((error)=>{
+          //console.log(error)
+        })
+
+    } else {
+      setSuccess(false);
+      setError(true);
+    }
   }
 
   return (
@@ -35,29 +43,38 @@ function App() {
           <Col style={{padding: "3rem", margin: "auto"}}>
               <h1>Distributed computing</h1>
               <p>The python code you submit will be distributed on a cluster of several workstations to improve performance.</p>
+              {error && 
+                <Alert variant="danger">The title or code field(s) are empty! Please fill in all the fields to submit your code.</Alert>
+              }
+              {success && 
+                <Alert variant="success">The code has been submitted!</Alert>
+              }
               <Form>
                 <Form.Group className="mb-3">
-                  <Form.Control type="file" accept=".py" onChange={handleFileSelect} />
+                  <Form.Control className="mb-4" placeholder="Enter script title here" type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <CodeEditor
+                    value={code}
+                    language="python"
+                    placeholder="Enter python code here."
+                    onChange={(e) => setCode(e.target.value)}
+                    padding={15}
+                    style={{
+                      borderRadius: 6,
+                      fontSize: 15,
+                      backgroundColor: "#1C1B22",
+                      fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+                      marginBottom: "1rem"
+                    }}
+                  />
                 </Form.Group>
                 <div>
                   <Button type="submit" variant="dark" style={{width:"30%"}} onClick={(e)=>submitCode(e)}>Submit</Button>
                 </div>
               </Form>
-              <h1 className="mt-5 mb-3">Results</h1>
-              <CodeEditor
-                value={code}
-                language="python"
-                placeholder="Please enter python code here."
-                onChange={(e) => setCode(e.target.value)}
-                padding={15}
-                style={{
-                  borderRadius: 6,
-                  fontSize: 15,
-                  backgroundColor: "#1C1B22",
-                  fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-                  marginBottom: "1rem"
-                }}
-              />
+              {/* <h1 className="mt-5 mb-3">Results</h1> */}
+              
           </Col>
         </Row>
       </Container>
