15dd8f8c3622531481ff71abdefee69a8002be50
Krosara <kristian.savov@gmail.com>
2022-05-20 12:32:24 +0200

    implemented file upload endpoint

---
+++ back-end/DcApi/DcApi/Controllers/FileController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace DcApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : Controller
+    {
+        [HttpPost("upload")]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            string tempPath = Path.GetTempPath();
+            string _fileName = Path.GetFileName(file.FileName);
+
+            if (!string.IsNullOrWhiteSpace(_fileName))
+            {
+                _fileName = _fileName.Replace(" ", "_");
+                string fileExtension = Path.GetExtension(_fileName);
+                string fileName = _fileName.TrimEnd(fileExtension.ToCharArray());
+
+                fileName = $"{fileName}_{Guid.NewGuid()}{fileExtension}";
+
+                using FileStream stream = new FileStream(Path.Combine(Path.GetTempPath(), fileName), FileMode.Create);
+                    file.CopyTo(stream);
+                    return Ok();
+
+            }
+
+            return BadRequest();
+        }
+    }
+}
