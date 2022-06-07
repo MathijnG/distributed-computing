80b8b81ef22b1c10388d748d4fe8ff83ad49eb14
Mathijs455 <455150@student.fontys.nl>
2022-06-07 11:12:38 +0200

    Add roles on startup

---
+++ back-end/DcApi/DcApi/Persistency/RoleData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DcApi.Persistency
+{
+    public static class RoleData
+    {
+        private static readonly string[] Roles = new string[] { "Admin", "Reader", "Writer" };
+
+        public static async Task SeedRoles(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                if (dbContext.Database.GetPendingMigrations().Any())
+                {
+                    await dbContext.Database.MigrateAsync();
+
+                    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    foreach (var role in Roles)
+                    {
+                        if (!await roleManager.RoleExistsAsync(role))
+                        {
+                            await roleManager.CreateAsync(new IdentityRole(role));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
