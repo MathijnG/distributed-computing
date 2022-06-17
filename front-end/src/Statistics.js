980d68b4e201c602174aaa68497c0fabe4c4fe40
Krosara <kristian.savov@gmail.com>
2022-06-17 17:23:02 +0200

    displaying stats in a table

--- front-end/src/Statistics.js
+++ front-end/src/Statistics.js
@@ -3,87 +3,52 @@ import { Table } from 'react-bootstrap';
 import axios from 'axios';
 
 const Statistics = () => {
-  const [doneApps, setDoneApps] = useState([]);
+  const [apps, setApps] = useState([]);
   const [temp, setTemp] = useState([]);
   const [data, setData] = useState([]);
 
-  const statsDone = [
-    {
-      id: 'application_1653992905362_0017',
-      name: '',
-      attempts: [
-        {
-          attemptId: '1',
-          startTime: '2022-06-03T08:24:49.966GMT',
-          endTime: '2022-06-03T08:25:52.235GMT',
-          lastUpdated: '2022-06-03T08:25:52.321GMT',
-          duration: 62269,
-          sparkUser: 'hadoop',
-          completed: true,
-          appSparkVersion: '3.2.1',
-          startTimeEpoch: 1654244689966,
-          endTimeEpoch: 1654244752235,
-          lastUpdatedEpoch: 1654244752321,
-        },
-      ],
-    },
-    {
-      id: 'application_1653393292774_0001',
-      name: 'PythonPi',
-      attempts: [
-        {
-          attemptId: '1',
-          startTime: '2022-05-31T08:41:10.381GMT',
-          endTime: '2022-05-31T08:41:45.197GMT',
-          lastUpdated: '2022-05-31T08:41:45.291GMT',
-          duration: 34816,
-          sparkUser: 'hadoop',
-          completed: true,
-          appSparkVersion: '3.2.1',
-          startTimeEpoch: 1653986470381,
-          endTimeEpoch: 1653986505197,
-          lastUpdatedEpoch: 1653986505291,
-        },
-      ],
-    },
-    {
-      id: 'application_1653992905362_0022',
-      name: '',
-      attempts: [
-        {
-          attemptId: '1',
-          startTime: '2022-06-07T08:24:49.966GMT',
-          endTime: '1969-12-31T23:59:59.999GMT',
-          lastUpdated: '2022-06-07T08:25:52.321GMT',
-          duration: 0,
-          sparkUser: 'hadoop',
-          completed: false,
-          appSparkVersion: '3.2.1',
-          startTimeEpoch: 1654244689966,
-          endTimeEpoch: -1,
-          lastUpdatedEpoch: 1654244752321,
-        },
-      ],
-    },
-  ];
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_SPARK_MONITOR + '/api/v1/applications')
+      .then((response) => setData(response.data));
+  }, []);
 
   useEffect(() => {
-    axios.get(process.env.REACT_APP_SPARK_MONITOR + '/api/v1/applications')
-      .then((response) => {
-        setData(response);
+    filterApps();
+  }, [data]);
 
-        data.forEach((app) => {
-          var index = data.indexOf(app);
-          if (!app.attempts[0].completed && !temp.includes(app)) {
-            temp.push(app);
-            data.splice(data.indexOf(index));
-          }
-        });
-        var final = temp.concat(data);
-        console.log(temp);
-        setDoneApps(final);
-      })
-  }, [data, temp]);
+  const filterApps = () => {
+    data.forEach((app) => {
+      var index = data.indexOf(app);
+      var newArray = [];
+      if (!app.attempts[0].completed && !Array.from(temp).includes(app)) {
+        // setTemp(...temp, app);
+        temp.push(app);
+        newArray = data.filter((i) => temp.includes(i));
+        console.log(newArray);
+      }
+    });
+    var final = [...temp, ...data];
+    setApps(final);
+  };
+
+  const renderTable = () => {
+    return apps.map((app) => {
+      return (
+        <tr>
+          <td key={app.id}>{app.id}</td>
+          <td key={app.name}>{app.name ? app.name : 'N/A'}</td>
+          <td key={app.attempts[0].startTime}>{app.attempts[0].startTime}</td>
+          <td key={app.attempts[0].endTime}>
+            {app.attempts[0].completed ? app.attempts[0].endTime : 'N/A'}
+          </td>
+          <td key={app.attempts[0].completed}>
+            {app.attempts[0].completed ? 'Completed' : 'Running'}
+          </td>
+        </tr>
+      );
+    });
+  };
 
   return (
     <Fragment>
@@ -102,25 +67,7 @@ const Statistics = () => {
               </tr>
             </thead>
 
-            {doneApps.map((app) => {
-              return (
-                <tbody>
-                  <tr>
-                    <td>{app.id}</td>
-                    <td>{app.name ? app.name : 'N/A'}</td>
-                    <td>{app.attempts[0].startTime}</td>
-                    <td>
-                      {app.attempts[0].completed
-                        ? app.attempts[0].endTime
-                        : 'N/A'}
-                    </td>
-                    <td>
-                      {app.attempts[0].completed ? 'Completed' : 'Running'}
-                    </td>
-                  </tr>
-                </tbody>
-              );
-            })}
+            <tbody>{renderTable()}</tbody>
           </Table>
         </div>
       ) : null}
